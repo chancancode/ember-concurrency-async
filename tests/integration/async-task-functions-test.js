c00fe216b110f295f97dfdb511cbbc753cb69748
James C. Davis <jamescdavis@gmail.com>
2020-06-20 05:23:34 -0400

    test: add tests for task wrapper

--- tests/integration/async-task-functions-test.js
+++ tests/integration/async-task-functions-test.js
@@ -83,4 +83,132 @@ module('Integration | async-task-functions', function(hooks) {
     assert.dom('#value').hasText('Done!');
     assert.dom('#resolved').hasText('Wow!');
   });
+
+  test('it works when using task wrapper', async function(assert) {
+    let { promise, resolve } = defer();
+
+    this.owner.register('component:test', class extends Component {
+      resolved = null;
+
+      @task myTask = task(async function(arg) {
+        set(this, 'resolved', await promise);
+        return arg;
+      });
+
+      @computed('myTask.performCount')
+      get isWaiting() {
+        return this.myTask.performCount === 0;
+      }
+
+      @computed('myTask.isRunning')
+      get isRunning() {
+        return this.myTask.isRunning;
+      }
+
+      @computed('myTask.last.value')
+      get value() {
+        return this.myTask.last.value;
+      }
+    });
+
+    this.owner.register('template:components/test', hbs`
+      {{#if this.isWaiting}}
+        <button id="start" {{on "click" (perform this.myTask "Done!")}}>Start!</button>
+      {{else if this.isRunning}}
+        Running!
+        {{else}}
+        Finished!
+        <span id="state">{{this.myTask.state}}</span>
+        <span id="value">{{this.value}}</span>
+        <span id="resolved">{{this.resolved}}</span>
+      {{/if}}
+    `);
+
+    await render(hbs`<Test />`);
+
+    assert.dom('button#start').hasText('Start!');
+    assert.dom().doesNotContainText('Running!');
+    assert.dom().doesNotContainText('Finished!');
+
+    await click('button#start');
+
+    assert.dom('button#start').doesNotExist();
+    assert.dom().containsText('Running!');
+    assert.dom().doesNotContainText('Finished!');
+
+    resolve('Wow!');
+
+    await settled();
+
+    assert.dom('button#start').doesNotExist();
+    assert.dom().doesNotContainText('Running!');
+    assert.dom().containsText('Finished!');
+    assert.dom('#state').hasText('idle');
+    assert.dom('#value').hasText('Done!');
+    assert.dom('#resolved').hasText('Wow!');
+  });
+
+  test('it works when using task wrapper with an arrow function', async function(assert) {
+    let { promise, resolve } = defer();
+
+    this.owner.register('component:test', class extends Component {
+      resolved = null;
+
+      @task myTask = task(async (arg) => {
+        set(this, 'resolved', await promise);
+        return arg;
+      });
+
+      @computed('myTask.performCount')
+      get isWaiting() {
+        return this.myTask.performCount === 0;
+      }
+
+      @computed('myTask.isRunning')
+      get isRunning() {
+        return this.myTask.isRunning;
+      }
+
+      @computed('myTask.last.value')
+      get value() {
+        return this.myTask.last.value;
+      }
+    });
+
+    this.owner.register('template:components/test', hbs`
+      {{#if this.isWaiting}}
+        <button id="start" {{on "click" (perform this.myTask "Done!")}}>Start!</button>
+      {{else if this.isRunning}}
+        Running!
+        {{else}}
+        Finished!
+        <span id="state">{{this.myTask.state}}</span>
+        <span id="value">{{this.value}}</span>
+        <span id="resolved">{{this.resolved}}</span>
+      {{/if}}
+    `);
+
+    await render(hbs`<Test />`);
+
+    assert.dom('button#start').hasText('Start!');
+    assert.dom().doesNotContainText('Running!');
+    assert.dom().doesNotContainText('Finished!');
+
+    await click('button#start');
+
+    assert.dom('button#start').doesNotExist();
+    assert.dom().containsText('Running!');
+    assert.dom().doesNotContainText('Finished!');
+
+    resolve('Wow!');
+
+    await settled();
+
+    assert.dom('button#start').doesNotExist();
+    assert.dom().doesNotContainText('Running!');
+    assert.dom().containsText('Finished!');
+    assert.dom('#state').hasText('idle');
+    assert.dom('#value').hasText('Done!');
+    assert.dom('#resolved').hasText('Wow!');
+  });
 });
